19a4c364cb6fdd5e4994157f622faf867c246c17
chowon <a39402819@gmail.com>
2025-09-17 11:10:05 +0900

    3주차 업로드

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
-package com.example.demo; // 현재폴더위치
-import org.springframework.boot.SpringApplication; // 스프링핵심클래스
-import org.springframework.boot.autoconfigure.SpringBootApplication; // 자동설정기능활성화
+package com.example.demo;
 
-@SpringBootApplication // 애노테이션(스프링부트APP 명시, 하위다양한설정을자동등록)
- public class DemoApplication { // 클래스이름
-public static void main(String[] args) { // 메인메서드(프로그램시작점)
- SpringApplication.run(DemoApplication.class, args); // run 메서드로실행
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
 }
- }
--- demo/src/main/java/com/example/demo/DemoController.java
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -5,9 +5,28 @@ package com.example.demo;
  @Controller // 컨트롤러 어노테이션 명시
 
  public class DemoController {
-    @GetMapping("/hello") // 전송 방식 GET
+    @GetMapping("/hello")
     public String hello(Model model) {
-    model.addAttribute("data", " 방갑습니다."); // model 설정
-    return "hello"; // hello.html 연결
+        model.addAttribute("greeting", "안녕하세요!");
+        model.addAttribute("message1", "방갑습니다.");
+        return "hello"; // hello.html 파일을 화면에 보여줍니다.
     }
+
+    // 브라우저에서 /hello2 주소로 접속하면 이 부분이 실행됩니다.
+    @GetMapping("/hello2")
+    public String hello2(Model model) {
+        model.addAttribute("greeting", "안녕하세요!");
+        model.addAttribute("username", "이초원님.");
+        model.addAttribute("message1", "방갑습니다.");
+        model.addAttribute("message2", "오늘.");
+        model.addAttribute("message3", "날씨는.");
+        model.addAttribute("message4", "매우 좋습니다.");
+        return "hello2"; // hello2.html 파일을 화면에 보여줍니다.
+    }
+     @GetMapping("/about_detailed")
+ public String about() {
+ return "about_detailed";
  }
+
+}
+
