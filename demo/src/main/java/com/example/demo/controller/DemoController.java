8a1d587cc5cdf8bf29e692e674e436feefd1b337
chowon <a39402819@gmail.com>
2025-09-24 11:21:10 +0900

    4주차

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.controller;
  import org.springframework.stereotype.Controller;
  import org.springframework.ui.Model;
  import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +26,17 @@ package com.example.demo;
      @GetMapping("/about_detailed")
  public String about() {
  return "about_detailed";
+ }
+
+ @GetMapping("/test1")
+ public String thymeleaf_test1(Model model) {
+ model.addAttribute("data1", "<h2> 방갑습니다 </h2>");
+ model.addAttribute("data2", "태그의 속성 값");
+ model.addAttribute("link", 01);
+ model.addAttribute("name", "이초원");
+ model.addAttribute("para1", "001");
+ model.addAttribute("para2", 002);
+ return "thymeleaf_test1";
  }
 
 }
