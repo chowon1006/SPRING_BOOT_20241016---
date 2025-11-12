1e44788dd9183d651944e50db8c405eb42e6fe0f
chowon <a39402819@gmail.com>
2025-11-12 13:01:24 +0900

    9주차

--- demo/src/main/java/com/example/demo/controller/BlogRestController.java
+++ demo/src/main/java/com/example/demo/controller/BlogRestController.java
@@ -1,10 +1,10 @@
  package com.example.demo.controller;
- import com.example.demo.model.domain.Article;
- import com.example.demo.model.service.AddArticleRequest;
+ //import com.example.demo.model.domain.Article;
+ //import com.example.demo.model.service.AddArticleRequest;
  import com.example.demo.model.service.BlogService;
  import lombok.RequiredArgsConstructor;
- import org.springframework.http.HttpStatus;
- import org.springframework.http.ResponseEntity;
+// import org.springframework.http.HttpStatus;
+ //import org.springframework.http.ResponseEntity;
  import org.springframework.web.bind.annotation.*;
 
    @RequiredArgsConstructor
@@ -12,15 +12,15 @@
    public class BlogRestController{
    private final BlogService blogService;
  
-   @PostMapping("/api/articles") // post 요청
-  public ResponseEntity<Article> addArticle(@ModelAttribute AddArticleRequest request) { // 아직없음(에러)
-  Article saveArticle= blogService.save(request); // 게시글저장
-  return ResponseEntity.status(HttpStatus.CREATED) // 상태코드및게시글정보반환
-    .body(saveArticle);
-   }
+//    @PostMapping("/api/articles") // post 요청
+//   public ResponseEntity<Article> addArticle(@ModelAttribute AddArticleRequest request) { // 아직없음(에러)
+//   Article saveArticle= blogService.save(request); // 게시글저장
+//   return ResponseEntity.status(HttpStatus.CREATED) // 상태코드및게시글정보반환
+//     .body(saveArticle);
+//    }
+
     @GetMapping("/favicon.ico")
    public void favicon() {
    // 아무 작업도 하지 않음
-   }
-
-   }
+      }
+    }
